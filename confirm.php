7f8cf07fab9122775bd6489c0ba3ce159685a202
MonicaIzzo <info@monicaizzo.net>
2023-07-16 08:56:09 +0200

    cread HTML

---
+++ confirm.php
@@ -0,0 +1,45 @@
+<?php
+$paragraph = $_POST['paragraph']; // Recuperare da un Form con il metodo GET i valori inseriti
+$censor = $_POST['censor'];    // Recuperare da un Form con il metodo GET i valori inseriti
+$paragraph_censor = '';
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Free Web tutorials">
+    <meta name="keywords" content="HTML, CSS">
+    <meta name="author" content="Monica Izzo">
+    <title>php Badwords | Pagina di ritorno</title>
+
+    <!-- icon-->
+    <link rel="icon" type="image/png" sizes="32x32" href="img/faicon.png">
+    <!-- stylesheet -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link href="css/style.css" rel="stylesheet">
+</head>
+
+<body>
+    <div class="container mx-0">
+        <!-- PARAGRAFO ORIGINALE -->
+        <div>
+            <h3 class="text-center p-5">
+                <?php echo $paragraph ?> </h3>
+        </div>
+    </div>
+    <!--PARAGRAFO CENSURATO-->
+    <div>
+        <h3 class="text-center p-5">
+            <?php echo $paragraph_censor ?> </h3>
+        <h5><?php var_dump('paragraph'); ?></h5>
+    </div>
+    </div>
+    </div>
+</body>
+
+</html>
--- index.php
+++ index.php
@@ -1,4 +1,3 @@
-
 <!--
 PHP Badwords
 nome repo: php-badwords
@@ -13,7 +12,97 @@ Il secondo file riceverà la richiesta ed eseguirà queste operazioni:
 
 -->
 
+<!DOCTYPE html>
+<html lang="en">
 
-<?php
-echo 'Ciao Classe 100';
-?>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Free Web tutorials">
+    <meta name="keywords" content="HTML, CSS">
+    <meta name="author" content="Monica Izzo">
+    <title>php Badwords | Pagina iniziale</title>
+
+    <!-- icon-->
+    <link rel="icon" type="image/png" sizes="32x32" href="img/faicon.png">
+
+    <!-- stylesheet -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+</head>
+
+<body>
+    <!-- FORM -->
+    <div class="container-md p-5 mx-auto mt-5 bg-primary text-white" id="cardData">
+        <h1 class="text-center mt-2">INSERISCI IL TUO PARAGRAFO</h1>
+        <!-- FORM -->
+        <form action="confirm.php" method="POST">
+            <section id="form-section">
+                <div class="card p-3 shadow mt-5 mb-5" id="card">
+                    <!-- FORM USER INPUT -->
+                    <div class="card-body">
+                        <!-- PARAGRAFO INPUT-->
+                        <div class="mb-3">
+                            <label class="form-label">Inserisci il tuo testo qui...</label>
+                            <textarea class="form-control" id="" rows="5" name="paragraph"></textarea>
+                        </div>
+                        <!-- PAROLA DA CENSURARE -->
+                        <div class="mb-3">
+                            <label class="form-label">Inserisci il termine da censurare...</label>
+                            <input type="text" class="form-control" id="" placeholder="Termine da censurare" name="censor">
+                        </div>
+                    </div>
+                    <!-- BUTTON -->
+                    <div class="container text-center button" id="button">
+                        <div class="row justify-content-center mt-3">
+                            <div class="col">
+                                <button type="submit" class="btn btn-primary btn-lg m-2" id="">Invia</button>
+                                <button type="reset" class="btn btn-outline-secondary btn-lg m-2" id="reset-button">Annulla</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </form>
+    </div>
+    </div>
+
+
+
+
+
+    <?php
+    /* 
+    echo 'Ciao Classe 100'; //Serve per stampare a schermo
+    var_dump('Ciao Classe 100'); //Serve avere delle info su una variabile
+    */
+
+
+    /* 
+    $first_name = 'Monica';
+    $last_name = 'Izzo';
+    $full_name = $first_name . ' ' . $last_name; //?Interpolazione di varibili - TIPO 1
+    echo "<h1>$full_name<h1>";
+    */
+
+
+    /* 
+    $first_name = 'Monica';
+    $last_name = 'Izzo';
+    $full_name = "$first_name $last_name"; //?Interpolazione di varibili - TIPO 2
+    echo "<h1>$full_name<h1>";
+    */
+
+
+    /* 
+    $presentazione = 'Ciao sono ';
+    $first_name = 'Monica';
+    $last_name = 'Izzo';
+    $full_name = $first_name . ' ' . $last_name;
+    $presentazione .= $full_name;
+    echo "<h1>$presentazione<h1>";
+    */
+    ?>
+</body>
+
+</html>
